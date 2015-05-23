bd22602ab7ef929dc9963f4bb01bf0ebeb7b4d31
creespow <crespo.alcorcon@gmail.com>
2015-05-23 16:26:38 +0200

    finished

--- practica_final/urls.py
+++ practica_final/urls.py
@@ -8,16 +8,18 @@ urlpatterns = patterns('',
     # url(r'^$', 'practica_final.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
+    url(r'^admin/', include(admin.site.urls)),
     url(r'^$','webapp.views.begin'),
     url(r'^static/(?P<path>.*)$', "django.views.static.serve",
 {'document_root': settings.STATIC_URL}),
 
-    url(r'^login/$','webapp.views.make_login'),
-    url(r'^private/$','webapp.views.private'),
-    url(r'^logout/$', 'webapp.views.make_logout'),
-
-
-
-
-    url(r'^admin/', include(admin.site.urls)),
+    url(r'^save/(.*)$','webapp.views.save'),
+    url(r'^makeparse/$','webapp.views.parser'),
+    url(r'^todas','webapp.views.all'),
+    url(r'^ayuda','webapp.views.help'),
+    url(r'^private','webapp.views.private'),
+    url(r'^login','webapp.views.make_login'),    
+    url(r'^logout', 'webapp.views.make_logout'),
+    url(r'^(.+)/rss', 'webapp.views.get_rss'),
+    url(r'^(.+)', 'webapp.views.userpage')
 )
